3f4e126d8ce9f603f19747c97a3ba898c29e20f7
freemanbach <flo@radford.edu>
2023-07-13 21:36:14 -0400

    added basic SHA ComboBox

--- Hashing/MainWindow.xaml.cs
+++ Hashing/MainWindow.xaml.cs
@@ -21,5 +21,38 @@ namespace Hashing {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void Form_Load(object sender, EventArgs e) {
+            load_shavalues();
+  
+
+        }
+        private void load_shavalues() {
+            shavalues.Items.Add("SHA-1");
+            shavalues.Items.Add("SHA-224");
+            shavalues.Items.Add("SHA-256");
+            shavalues.Items.Add("SHA-384");
+            shavalues.Items.Add("SHA-512");
+            shavalues.Items.Add("SHA3-224");
+            shavalues.Items.Add("SHA3-256");
+            shavalues.Items.Add("SHA3-384");
+            shavalues.Items.Add("SHA3-512");
+            shavalues.Items.Add("SHAKE-128");
+            shavalues.Items.Add("SHAKE-256");
+            shavalues.Items.Add("cSHAKE-128");
+            shavalues.Items.Add("cSHAKE-256");
+            shavalues.Items.Add("TupleHash-128");
+            shavalues.Items.Add("TupleHash-256");
+            shavalues.Items.Add("ParallelHash-128");
+            shavalues.Items.Add("ParallelHash-256");
+        }
+
+        private void shavalues_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e) {
+            Environment.Exit(0);
+        }
     }
 }
